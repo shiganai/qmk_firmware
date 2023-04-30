92f5c897b4f73f810f45ad99a07a239063039501
shiganai <shiganai.hiro@gmail.com>
2023-04-30 16:13:39 +0900

    [Ergodashmini Keymap] Customized for Mac.

--- keyboards/omkbd/ergodash/mini/keymaps/shiganai/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/shiganai/keymap.c
@@ -5,17 +5,37 @@ enum layer_names {
     _QWERTY,
     _NUMBR,
     _FNCTN,
-    _ADJUST
+    _ADJUST,
+    _MacQWERTY,
+    _MacNUMBR,
+    _MacFNCTN,
 };
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  NUMBR,
-  FNCTN,
   ADJUST,
+  Tg_IME,
+  KC_WLC,	//Windows-Like Control. Basically work as LGUI(Command). When arrow button pressed, work as Ctrl.
+  MC_LEFT,	//Left arrow working with KC_WLRW
+  MC_RGHT,	//Right arrow working with KC_WLRW
+  MC_UP,	//Up arrow working with KC_WLRW
+  MC_DOWN,	//Down arrow working with KC_WLRW
 };
 
 #define EISU LALT(KC_GRV)
+
+#define NUMBR MO(_NUMBR)
+#define FNCTN MO(_FNCTN)
+#define SwMac DF(_MacQWERTY)	//Toggle on Mac QWERTY
+#define SwWin DF(_QWERTY)	//Toggle on Win QWERTY
+#define McNUM MO(_MacNUMBR)	//Toggle on Mac Number
+#define McFUN MO(_MacFNCTN)	//Toggle on Mac Function
+
+#define MC_HOME LGUI(KC_LEFT)	//Home for Mac
+#define MC_END LGUI(KC_RGHT)	//End for Mac
+#define MC_PGUP LGUI(KC_UP)	//PageUP for Mac
+#define MC_PGDN LGUI(KC_DOWN)	//PageDown for Mac
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -23,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  Esc |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   A  |   S  |   D  |   F  |   G  |   /  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Tab  |   A  |   S  |   D  |   F  |   G  |   /  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | CAPSL|                    |Delete|   N  |   M  |   ,  |   .  |  Up  | Bksp |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -41,84 +61,169 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |   `  |   !  |   @  |   #  |   $  |   %  |   {  |                    |   }  |   ^  |   &  |   *  |   (  |   )  |  |   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |   ~  |   1  |   2  |   3  |   4  |   5  |   ?  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * |   ~  |   1  |   2  |   3  |   4  |   5  |   ?  |                    |   =  |   H  |   J  |   K  |   L  |   :  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   6  |   7  |   8  |   9  |   0  | CAPSL|                    |Delete|   N  |   M  |   ,  |   .  |PageUp| Bksp |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Numbr| Space|   -  |||||||| Shift| Enter| Fnctn|||||||| Menu | Home |PageDn|  End |
+   * | Ctrl |  GUI |  ALt |Tg_IME|||||||| Fnctn| Space|   -  |||||||| Shift| Enter| Numbr|||||||| Menu | Home |PageDn|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_NUMBR] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQUO,
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_PGUP, KC_BSPC,
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             NUMBR,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , FNCTN,            KC_APP , KC_HOME, KC_PGDN, KC_END
+    KC_LCTL, KC_LGUI, KC_LALT, EISU,             FNCTN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , NUMBR,            KC_APP , KC_HOME, KC_PGDN, KC_END
   ),
 
   /* Fnctn
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |   `  |   !  |   @  |   #  |   $  |   %  |   {  |                    |   }  |   ^  |   &  |   *  |   (  |   )  |  |   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |   ~  |   F1 |   F2 |   F3 |   F4 |   F5 |   ?  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | SwMac|   F1 |   F2 |   F3 |   F4 |   F5 |   ?  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   F6 |   F7 |   F8 |   F9 |  F10 | CAPSL|                    |Delete|   N  |   M  |   ,  |   .  |PageUp| Bksp |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  F11 |  F12 | EISU |||||||| Numbr| Space|   -  |||||||| Shift| Enter| Fnctn|||||||| Menu | Home |PageDn|  End |
+   * | Ctrl |  F11 |  F12 | SwWin|||||||| Fnctn| Space|   -  |||||||| Shift| Enter| Numbr|||||||| Menu | Home |PageDn|  End |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_FNCTN] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    SwMac,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQUO,
     KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_PGUP, KC_BSPC,
-    KC_LCTL, KC_F11 , KC_F12,  EISU,             NUMBR,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , FNCTN,            KC_APP , KC_HOME, KC_PGDN, KC_END
+    KC_LCTL, KC_F11 , KC_F12,  SwWin,            FNCTN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , NUMBR,            KC_APP , KC_HOME, KC_PGDN, KC_END
     ),
 
-  /* Adjust
+  /* MacQWERTY	
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |      | Reset|RGB ON|  MODE|  HUE-|  HUE+|      |                    |      |  SAT-|  SAT+|  VAL-|  VAL+|      |      |
+   * |  Esc |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | Tab  |   A  |   S  |   D  |   F  |   G  |   /  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  '   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
-   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | CAPSL|                    |Delete|   N  |   M  |   ,  |   .  |  Up  | Bksp |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * |      |      |      |      ||||||||      |      |      ||||||||      |      |      ||||||||      |      |      |      |
+   * | Ctrl |  GUI |  ALt |Tg_IME|||||||| Fnctn| Space|   -  |||||||| Shift| Enter| Numbr|||||||| Menu | Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
-  [_ADJUST] = LAYOUT(
-    _______, QK_BOOT  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                       _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______,
-    _______, _______, BL_TOGG, BL_BRTG, BL_UP  , BL_DOWN,_______,                       _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______
-  )
+  [_MacQWERTY] = LAYOUT(
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_SLSH,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP  , KC_BSPC,
+    KC_WLC , KC_LCTL, KC_LALT, Tg_IME,           McFUN,   KC_SPC, KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_LEFT, KC_DOWN, MC_RGHT
+  ),
+  
+  // NOTE: Need to duplicate Number and Function layers for Mac because the original ones won't be activated via _MacBase layer.
+
+  /* MacNumbr
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |   `  |   !  |   @  |   #  |   $  |   %  |   {  |                    |   }  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * |   ~  |   1  |   2  |   3  |   4  |   5  |   ?  |                    |   =  |   H  |   J  |   K  |   L  |   :  |  "   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   6  |   7  |   8  |   9  |   0  | CAPSL|                    |Delete|   N  |   M  |   ,  |   .  |PageUp| Bksp |
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  GUI |  ALt |Tg_IME|||||||| Fnctn| Space|   -  |||||||| Shift| Enter| Numbr|||||||| Menu | Home |PageDn|  End |
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  [_MacNUMBR] = LAYOUT(
+    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQUO,
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  MC_PGUP, KC_BSPC,
+    KC_LGUI, KC_LCTL, KC_LALT, Tg_IME,           McFUN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_HOME, MC_PGDN, MC_END
+  ),
+
+  /* MacFnctn
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   * |   `  |   !  |   @  |   #  |   $  |   %  |   {  |                    |   }  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+   * | SwMac|   F1 |   F2 |   F3 |   F4 |   F5 |   ?  |                    |   =  |   H  |   J  |   K  |   L  |   :  |  "   |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * | Shift|   F6 |   F7 |   F8 |   F9 |  F10 | CAPSL|                    |Delete|   N  |   M  |   ,  |   .  |PageUp| Bksp |
+   * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+   * | Ctrl |  F11 |  F12 | SwWin|||||||| Fnctn| Space|   -  |||||||| Shift| Enter| Numbr|||||||| Menu | Home |PageDn|  End |
+   * ,----------------------------------------------------------------------------------------------------------------------.
+   */
+  [_MacFNCTN] = LAYOUT(
+    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+    SwMac,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQUO,
+    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  MC_PGUP, KC_BSPC,
+    KC_LCTL, KC_F11 , KC_F12,  SwWin,            McFUN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_HOME, MC_PGDN, MC_END
+    ),
 };
+
+static bool IME = false;
+
+static bool WLC = false;
+#define default_WLC KC_LGUI
+#define RLarrow_special_WLC KC_LALT
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case NUMBR:
+  
+    // For Mac Right and Left Customize
+    case KC_WLC:
       if (record->event.pressed) {
-        layer_on(_NUMBR);
-        update_tri_layer(_NUMBR, _FNCTN, _ADJUST);
+        // Turn on WLC flag so that MC_LEFT and MC_RGHT is aware of which action to take.
+        // Also turn on Command(LGUI) as default_WLC
+      	WLC = true;
+        register_code(default_WLC);
       } else {
-        layer_off(_NUMBR);
-        update_tri_layer(_NUMBR, _FNCTN, _ADJUST);
+        WLC = false;
+        unregister_code(default_WLC);
       }
       return false;
       break;
-    case FNCTN:
+      
+    case MC_LEFT:
       if (record->event.pressed) {
-        layer_on(_FNCTN);
-        update_tri_layer(_NUMBR, _FNCTN, _ADJUST);
+        if (WLC) {
+          unregister_code(default_WLC);
+          register_code(RLarrow_special_WLC);
+        }
+        register_code(KC_LEFT);
       } else {
-        layer_off(_FNCTN);
-        update_tri_layer(_NUMBR, _FNCTN, _ADJUST);
+        unregister_code(KC_LEFT);
+        if (WLC) {
+          unregister_code(RLarrow_special_WLC);
+          register_code(default_WLC);
+        }
       }
       return false;
       break;
-    case ADJUST:
+      
+    case MC_RGHT:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        if (WLC) {
+          unregister_code(default_WLC);
+          register_code(RLarrow_special_WLC);
+        }
+        register_code(KC_RGHT);
       } else {
-        layer_off(_ADJUST);
+        unregister_code(KC_RGHT);
+        if (WLC) {
+          unregister_code(RLarrow_special_WLC);
+          register_code(default_WLC);
+        }
+      }
+      return false;
+      break;
+      
+      
+    // For IME
+    case Tg_IME:
+      if (record->event.pressed) {
+        if (IME) {
+          register_code(KC_LNG1);
+        } else {
+          register_code(KC_LNG2);
+        }
+        
+      } else {
+        if (IME) {
+          unregister_code(KC_LNG1);
+        } else {
+          unregister_code(KC_LNG2);
+        }
+        IME = !IME;
       }
       return false;
       break;
