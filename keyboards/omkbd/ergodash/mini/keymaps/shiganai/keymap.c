63f20dd16fdde271999d1127add15ba2a8d8703b
shiganai <shiganai.hiro@gmail.com>
2023-05-01 22:53:16 +0900

    [Ergodashmini Keymap] Apply KC_WLC to special layers

--- keyboards/omkbd/ergodash/mini/keymaps/shiganai/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/shiganai/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
     KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQUO,
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  MC_PGUP, KC_BSPC,
-    KC_LGUI, KC_LCTL, KC_LALT, Tg_IME,           McFUN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_HOME, MC_PGDN, MC_END
+    KC_WLC, KC_LCTL, KC_LALT, Tg_IME,           McFUN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_HOME, MC_PGDN, MC_END
   ),
 
   /* MacFnctn
@@ -146,7 +146,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
     SwMac,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_QUES,                        KC_EQL , KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_DQUO,
     KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_CAPS,                        KC_DEL , KC_N,    KC_M,    KC_COMM, KC_DOT,  MC_PGUP, KC_BSPC,
-    KC_LCTL, KC_F11 , KC_F12,  SwWin,            McFUN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_HOME, MC_PGDN, MC_END
+    KC_WLC, KC_F11 , KC_F12,  SwWin,            McFUN,   KC_SPC ,KC_MINS,        KC_RSFT,KC_ENT , McNUM,            KC_APP , MC_HOME, MC_PGDN, MC_END
     ),
 };
 
